6b7ed3c9f2a528c79e723fb02c055ca192f79ac2
Parth Pandey <119426977+parthvasu2004@users.noreply.github.com>
2025-02-24 10:47:30 +0530

    app.py

--- app.py
+++ app.py
@@ -6,13 +6,9 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
-# Download NLTK data if not already downloaded
-nltk_data_path = os.path.join(os.path.expanduser('~'), 'nltk_data')
-nltk.data.path.append(nltk_data_path)
-if not os.path.exists(os.path.join(nltk_data_path, 'tokenizers/punkt')):
-    nltk.download('punkt', download_dir=nltk_data_path)
-if not os.path.exists(os.path.join(nltk_data_path, 'corpora/stopwords')):
-    nltk.download('stopwords', download_dir=nltk_data_path)
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
 
 ps = PorterStemmer()
 
